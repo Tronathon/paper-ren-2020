5b92cb6b8d01fc642aac1b5944150501c72c8afe
Ryan <ryan@nixondesign.com>
2019-08-06 13:21:18 +0100

    Update cookie notice to align with current practice

--- src/scripts/modules/cookie-notice.js
+++ src/scripts/modules/cookie-notice.js
@@ -1,104 +1,16 @@
-import { getCookie, setCookie } from 'tiny-cookie';
+import Cookies from 'js-cookie';
 
-const defaults = {
-	notice: document.getElementById('ckntc'),
-	allowButton: document.getElementById('ckntc-y'),
-	disallowButton: document.getElementById('ckntc-n'),
+const notice = document.getElementById('cookie-notice');
+const controls = Array.from(document.querySelectorAll('.js-cookie-consent'));
+const className = 'is-visible';
+const cookieName = 'cookie_consent';
 
-	classList: 'is-visible',
-	cookieName: 'cookie_consent',
-	dataAttribute: 'data-cookie-consent',
-};
+if (notice && !Cookies.get(cookieName)) {
+	notice.classList.add(className);
+	controls.forEach(control => control.addEventListener('click', e => onClick(e)));
+}
 
-/**
- * Cookie Consent
- *
- * @param {Object} opts The configuration object
- */
-export default function CookieConsent(opts) {
-	let consented = false;
-
-	const config = {};
-
-	Object.assign(config, defaults, opts);
-
-	let elements = [...document.querySelectorAll(`[${config.dataAttribute}]`)];
-
-	config.allowButton.addEventListener('click', allow);
-	config.disallowButton.addEventListener('click', disallow);
-
-	switch (getCookie(config.cookieName)) {
-		case 'true':
-			hide();
-			loadContent();
-			consented = true;
-			break;
-		case 'false':
-			hide();
-			break;
-		default:
-			show();
-	}
-
-	function allow() {
-		hide();
-		loadContent();
-		setConsentCookie(true);
-
-		if (typeof dataLayer !== 'undefined') {
-			// Fire Google Tag Manager event
-			dataLayer.push({ 'event': 'cookie_consent_given' });
-		}
-	}
-
-	function disallow() {
-		hide();
-		setConsentCookie(false);
-	}
-
-	/**
-	 * Show the cookie notice
-	 */
-	function show() {
-		config.notice.classList.add(config.classList);
-	}
-
-	/**
-	 * Hide the cookie notice
-	 */
-	function hide() {
-		config.notice.classList.remove(config.classList);
-	}
-
-	/**
-	 * Set the consent cookie
-	 *
-	 * @param {Boolean} allow
-	 */
-	function setConsentCookie(allow) {
-		setCookie(config.cookieName, allow, { expires: '12M' });
-	}
-
-	/**
-	 * Load scripts, iframes etc that require consent
-	 */
-	function loadContent() {
-		elements.forEach(elem => {
-			const tagName = elem.tagName;
-			const attrValue = elem.getAttribute(config.dataAttribute);
-
-			if (attrValue && (tagName === 'SCRIPT' || tagName === 'IFRAME')) {
-				// External scripts and iframes
-				elem.src = attrValue;
-			} else if (tagName === 'SCRIPT' && elem.type === 'text/plain' ) {
-				// Inline scripts
-				var s = document.createElement('script');
-				s.innerHTML = elem.innerHTML;
-				elem.parentNode.insertBefore(s, elem);
-				elem.remove();
-			}
-
-			elem.removeAttribute(config.dataAttribute);
-		});
-	}
+function onClick(event) {
+	notice.classList.remove(className);
+	Cookies.set(cookieName, 'true', { expires: 365 });
 }
