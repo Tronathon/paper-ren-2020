7b449520e10b3f79b5a95402a0f8a86aec05d384
Ryan Pascoe <ryan@nixondesign.com>
2020-06-15 08:32:03 +0100

    Align with offical Craft boilerplate

--- config/general.php
+++ config/general.php
@@ -4,18 +4,22 @@
  *
  * All of your system's general configuration settings go in here. You can see a
  * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ *
+ * @see \craft\config\GeneralConfig
  */
+
+use craft\helpers\App;
 
 return [
     '*' => [
-		'securityKey' => getenv('SECURITY_KEY'),
+		'securityKey' => App::env('SECURITY_KEY'),
 		'sendPoweredByHeader' => false,
 		'useEmailAsUsername' => true,
         'autoLoginAfterAccountActivation' => true,
         'cpTrigger' => 'admin',
         'defaultCpLanguage' => 'en-GB',
         'omitScriptNameInUrls' => true,
-        'siteUrl' => getenv('SITE_URL'),
+        'siteUrl' => App::env('SITE_URL'),
         'useProjectConfigFile' => true,
     ],
 
